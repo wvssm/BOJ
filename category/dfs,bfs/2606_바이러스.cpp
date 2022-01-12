5f66a8d9bdfcdc9eb7e95daa2120e00c6ca8e9e3
wvssm <52875244+wvssm@users.noreply.github.com>
2022-01-13 01:50:37 +0900

    boj

---
+++ category/dfs,bfs/2606_바이러스.cpp
@@ -0,0 +1,54 @@
+﻿// 2606_바이러스
+// dfs, bfs 관련문제
+
+//디버깅 : 
+// 전파 바이러스를 퍼뜨린 컴퓨터를 제외하고 감염된 컴퓨터 수 만 출력해야되는데
+// 감염을 퍼뜨린 컴퓨터까지 cnt에 포함하였다. 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 100 + 1;
+
+vector<int> a[MAX];
+int c[MAX];
+int cnt = 0;
+
+void insert_node(int i, int j) {
+    a[i].push_back(j);
+    a[j].push_back(i);
+}
+
+void dfs(int start) {
+    if (c[start]) return;
+
+    c[start] = true;
+    cnt++;
+
+    for (int i = 0; i < a[start].size(); i++) {
+        int y = a[start][i];
+        dfs(y);
+    }
+}
+
+int main()
+{
+    int n, m, x, y;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> x >> y;
+        insert_node(x, y);
+    }
+
+    for (int i = 1; i <= n; i++) { // 번호가 작은 노드 순서대로 돌도록 vector를 정렬해 줌
+        sort(a[i].begin(), a[i].end());
+    }
+
+    dfs(1);
+
+    cout << cnt-1;
+}
---
+++ category/dfs,bfs/2667_단지번호붙이기.cpp
@@ -0,0 +1,79 @@
+﻿// 2667_단지번호붙이기
+// 참고 : 유셩장님, https://sihyungyou.github.io/baekjoon-2667/
+
+/*
+    문제 :  https://www.acmicpc.net/problem/2667
+
+    접근 : 지도의 모든 곳을 탐색해야 한다.
+    기존의 트리구조와 달리 연결구조가 상하좌우로 이루어져 있다.
+
+    디버깅 : 기존의 dfs를 다룰 때처럼 상하좌우 없이 단순히 한 선으로 
+    연결된 노드로 된 트리구조로 생각하여 vector 형식의 노드로 연결하여 만들어 실패하였다.
+    하지만 여기 문제에서 아파트는 상하좌우로 연결되어 있으며
+    이를 모두 체크하도록 dfs 함수를 작성해야 한다.
+
+    지도의 모든 곳을 탐색하고 그곳에 아파트가 있으면 그자리에서 dfs를 실행하여
+    상하좌우를 체크해 또 아파트가 있으면 재귀적으로 dfs를 실행하도록 코드를 작성하였다.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 25;
+
+int dx[4] = { 0,1,0,-1 }; // 상하좌우 검사
+int dy[4] = { -1,0,1,0 };
+
+string arr[MAX];
+
+int vis[MAX][MAX]; // 방문 여부 판단
+int n, cnt;
+vector<int> ans; // 답을 담는 벡터
+
+void dfs(int i, int j) {
+    vis[i][j] = true; 
+    cnt++;
+
+    for (int k = 0; k < 4; k++) { // 상하좌우 검사
+        int newY = i + dy[k];
+        int newX = j + dx[k];
+
+        if (0 <= newX && newX < n && 0 <= newY && newY < n) { // 해당영역 안에 들어가 있고, 아파트가 있고, 방문하지 않은 지역이면
+            if (arr[newY][newX] == '1' && !vis[newY][newX]) {
+                dfs(newY, newX); // 재귀적으로 dfs 실행
+            }
+        }
+    }
+}
+
+int main()
+{
+    int i, j;
+    cin >> n;
+
+    for (i = 0; i < n; i++) { // string으로 입력 받아 들임
+        cin >> arr[i];
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (arr[i][j] == '1' && !vis[i][j]) { 
+                cnt = 0;
+                dfs(i, j); // 아파트가 있으면 dfs 탐색 시작
+                ans.push_back(cnt);
+            }
+        }
+    }
+
+    cout << ans.size() << endl;
+    
+    sort(ans.begin(), ans.end());
+
+    for (i = 0; i < ans.size(); i++) {
+        cout << ans[i] << endl;
+    }
+
+    return 0;
+}
